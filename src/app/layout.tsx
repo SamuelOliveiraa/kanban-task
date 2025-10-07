395001babb00a300e9b0af7946990e629e306793
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2025-10-07 13:12:45 -0300

    chore: Adicionando um metadata melhor para o projeto.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,21 @@ export const metadata: Metadata = {
   title: "Kanban Task",
   description:
     "Organize suas tarefas com nosso Kanban Task Online. Visualize, arraste e acompanhe o progresso de um ou mais projetos de forma simples e intuitiva.",
-  icons: {
-    icon: "/favicon.ico"
+  robots: "index, follow",
+  openGraph: {
+    title: "Kanban Task",
+    description: "Organize suas tarefas com nosso Kanban Task Online.",
+    url: "https://kanban-task-samuel.vercel.app/og-image.webp",
+    siteName: "Kanban Task",
+    locale: "pt-BR",
+    images: [
+      {
+        url: "https://kanban-task-samuel.vercel.app/og-image.webp",
+        width: 800,
+        height: 600,
+        alt: "Kanban Task"
+      }
+    ]
   }
 };
 
